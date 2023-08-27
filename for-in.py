20214665830b5c8e31d1b8ec8c289b37dce9f4ba
Pinkset <143222511+Pinkset@users.noreply.github.com>
2023-08-26 21:42:16 -0300

    While contando numero de tentativas.

---
+++ for-in.py
@@ -0,0 +1,14 @@
+
+senha_salva = '123456'
+senha_digitada = ''
+repeticoes = 0
+
+while senha_salva != senha_digitada:
+    senha_digitada = input(f'Sua senha ({repeticoes})x: ')
+     
+    repeticoes += 1
+
+print(repeticoes)
+print('Poderia ter tido repetições infinitas.')
+                           
+
